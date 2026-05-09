cdfff2c0714df772cc5cbf966f5a1a025e3fd586
Ghnkz <49773907+Ghnkz@users.noreply.github.com>
2026-05-09 22:43:30 +0700

    Modify kernel configuration and source settings
    Updated kernel configuration and source paths.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,14 +102,9 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_RAMDISK_USE_LZ4 := true
 
-TARGET_KERNEL_CLANG_VERSION := banana
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := vendor/sweet_perf_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
-KERNEL_SUPPORTS_LLVM_TOOLS := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 LLVM_IAS=1
-KERNEL_LD := LD=ld.lld
+
+TARGET_KERNEL_CONFIG := sweet_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
