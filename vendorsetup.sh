1830b8820ec85636efeeeb2c845dab0f1861b8eb
Ghnkz <49773907+Ghnkz@users.noreply.github.com>
2026-05-09 23:03:12 +0700

    Add vendorsetup.sh to clone project dependencies
    This script clones various dependencies for the Xiaomi project, including vendor and kernel repositories.

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Cloning dependencies
+git clone https://github.com/Ghnkz/vendor_los -b ven13 vendor/xiaomi/sweet
+
+git clone https://github.com/manipvlator/android_kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+
+git clone https://gitlab.com/unsatifsed27/miuicamera -b leica vendor/xiaomi/sweet-miuicamera
+
+git clone https://github.com/Ghnkz/sony_vendor_dolby vendor/dolby
+
+rm -rf hardware/xiaomi && git clone https://github.com/Ghnkz/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
